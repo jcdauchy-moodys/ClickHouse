001c7ce70533573f63f0b80ea2cd9493531f075b
Vasily Nemkov <V.Nemkov@gmail.com>
2025-06-12 21:44:15 +0200

    Merge pull request #854 from Altinity/fixes/antlya-25.3_version_fix
    25.3 Antalya version fix

--- src/Common/SignalHandlers.cpp
+++ src/Common/SignalHandlers.cpp
@@ -583,7 +583,7 @@ try
         }
 
         /// Advice the user to send it manually.
-        if (std::string_view(VERSION_OFFICIAL).contains("official build"))
+        if (std::string_view(VERSION_OFFICIAL).contains("altinity build"))
         {
             /// Approximate support period, upper bound.
             if (time(nullptr) - makeDate(DateLUT::instance(), 2000 + VERSION_MAJOR, VERSION_MINOR, 1) < (365 + 30) * 86400)
