a10bf2bef79871015a5b67c613e2bced8d4a58c8
strtgbb <146047128+strtgbb@users.noreply.github.com>
2025-09-04 17:06:36 -0400

    disable unused log tables for fuzzers

--- ci/jobs/scripts/fuzzer/run-fuzzer.sh
+++ ci/jobs/scripts/fuzzer/run-fuzzer.sh
@@ -58,8 +58,8 @@ EOL
     <core_path>$PWD</core_path>
 </clickhouse>
 EOL
-
-    (cd $repo_dir && python3 $repo_dir/ci/jobs/scripts/clickhouse_proc.py logs_export_config) || { echo "Failed to create log export config"; exit 1; }
+    # NOTE (strtgbb): Log tables are disabled, we don't use them
+    # (cd $repo_dir && python3 $repo_dir/ci/jobs/scripts/clickhouse_proc.py logs_export_config) || { echo "Failed to create log export config"; exit 1; }
 }
 
 function filter_exists_and_template
@@ -183,7 +183,8 @@ function fuzz
 
     echo 'Server started and responded.'
 
-    (cd $repo_dir && python3 $repo_dir/ci/jobs/scripts/clickhouse_proc.py logs_export_start) || { echo "Failed to start log exports"; exit 1; }
+    # NOTE (strtgbb): Log tables are disabled, we don't use them
+    # (cd $repo_dir && python3 $repo_dir/ci/jobs/scripts/clickhouse_proc.py logs_export_start) || { echo "Failed to start log exports"; exit 1; }
 
     # Setup arguments for the fuzzer
     FUZZER_OUTPUT_SQL_FILE=''
